d53d956fdcd7d582dbc7c31e5f58499846641f40
PARKMINJUN <junee0530@naver.com>
2023-02-10 18:28:47 +0900

    index 페이지 다시 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,8 @@ import React from "react"
 const IndexPage = () => {
   return (
     <main>
-      <title>Home Page???</title>
-      <h1>Home page!!!!!!!</h1>
+      <title>Home Page</title>
+      <h1>Home page</h1>
     </main>
   )
 }
