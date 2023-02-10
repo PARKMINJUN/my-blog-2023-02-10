769134f43a613a0ddecaaf37ab4e636489da4516
PARKMINJUN <junee0530@naver.com>
2023-02-10 23:12:38 +0900

    Head 컴포넌트 추가하여, 쓸데없는 소스코드 중복제거

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,13 @@
 import { Link } from "gatsby"
 import React from "react"
+import Head from "../components/head"
 
 const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
       
-      <div style={{display:"flex", gap:10}}>
-            <Link to="/">홈</Link>
-            <Link to="/about">어바웃</Link>
-            <Link to="/test">테스트</Link>
-            <Link to="/test-space">테스트-스페이스</Link>
-        </div>
+      <Head />
         
       <h1>Home page</h1>
     </main>
