2e80bbe077f8941acc22c5c1c575f5dc785bf28c
PARKMINJUN <junee0530@naver.com>
2023-02-17 14:26:53 +0900

    블로그 페이지 추가

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -39,6 +39,11 @@ function Layout({ pageTitle , children }) {
                 About
               </Link>
             </li>
+            <li className={navLinkItem}>
+              <Link className={navLinkText} to="/blog">
+                Blog
+              </Link>
+            </li>
           </ul>
         </nav>
 
